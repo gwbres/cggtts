3d650c9304d115bcc3c9c28c7b0d715f6c0fa082
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-07-02 13:20:13 +0200

    test advanced data set

--- tests/cggtts.rs
+++ tests/cggtts.rs
@@ -56,36 +56,31 @@ mod test {
                     "Cggtts::from_file() failed for {:#?} with {:#?}",
                     path,
                     cggtts);
+                let cggtts = cggtts.unwrap();
+                assert_eq!(cggtts.has_ionospheric_data(), false);
             }
         }
     }
-    /*
     #[test]
-    fn from_advanced_data() {
+    fn parse_advanced_data() {
         let test_resources = std::path::PathBuf::from(
-            env!("CARGO_MANIFEST_DIR").to_owned() + "/data/ionospheric");
+            env!("CARGO_MANIFEST_DIR").to_owned() + "/data/advanced");
         for entry in std::fs::read_dir(test_resources)
             .unwrap() {
             let entry = entry
                 .unwrap();
             let path = entry.path();
             if !path.is_dir() { // only files..
-                let fp = std::path::Path::new(&path);
-                let cggtts = Cggtts::from_file(&fp);
+                let cggtts = Cggtts::from_file(&path.to_str().unwrap());
                 assert_eq!(
-                    cggtts.is_err(), 
+                    cggtts.is_err(),
                     false,
                     "Cggtts::from_file() failed for {:#?} with {:#?}",
-                    path, 
+                    path,
                     cggtts);
-                println!("File \"{:?}\" {:#?}", &path, cggtts.unwrap())
+                let cggtts = cggtts.unwrap();
+                assert_eq!(cggtts.has_ionospheric_data(), true);
             }
         }
     }
-
-    #[test]
-    fn default_cggtts_to_file() {
-        let cggtts = Cggtts::default();
-        assert_eq!(cggtts.to_file("data/output/GZXXXXDD.DD0").is_err(), false)
-    }*/
 }
