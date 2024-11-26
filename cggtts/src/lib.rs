bbc5bd7106d9c841255df0b587f0c8ab703a279d
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-11-26 09:11:43 +0100

    Update dependencies
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- cggtts/src/lib.rs
+++ cggtts/src/lib.rs
@@ -1,5 +1,5 @@
 #![doc = include_str!("../README.md")]
-#![cfg_attr(docrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 //! CGGTTS is the core structure, it comprises
 //! the list of tracks (measurements) and some header information.
 //! ```
